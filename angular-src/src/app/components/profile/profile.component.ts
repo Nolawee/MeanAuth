5efe69b8c85f075350d80dd774fc25f33fdfe476
Nolawee Mengist <nome9478@colorado.edu>
2017-04-16 19:41:29 -0600

    Protected Requests & Auth Guard

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: Object;
+  constructor(private authService: AuthService, private router: Router) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe(profile => {
+      console.log(this.user);
+      this.user = profile.user;
+    },
+      err => {
+        console.log(err);
+        return false;
+      });
   }
 
 }
